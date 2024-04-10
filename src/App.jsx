cd5205ccac0d7630a65b15f5e21682f5d7cdc229
aguspeiretti <aguspeiretti@gmail.com>
2024-04-10 12:55:52 -0300

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Calendar, dayjsLocalizer } from "react-big-calendar";
-import "./App.css";
-import "react-big-calendar/lib/css/react-big-calendar.css";
 import dayjs from "dayjs";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./App.css";
 import "dayjs/locale/es";
 
 function App() {
@@ -12,47 +12,6 @@ function App() {
     },
     timeslots: 3, // Intervalos de 20 minutos (3 intervalos por hora)
   });
-
-  const events = [
-    {
-      start: dayjs("2024-04-09T11:00:00").toDate(),
-      end: dayjs("2024-04-09T11:10:00").toDate(),
-      title: "anna.aguiTU5 - G - Conferencia",
-    },
-    {
-      start: dayjs("2024-04-09T11:00:00").toDate(),
-      end: dayjs("2024-04-09T11:10:00").toDate(),
-      title: "anna.aguiTU5 - G - Conferencia",
-    },
-    {
-      start: dayjs("2024-04-09T12:00:00").toDate(),
-      end: dayjs("2024-04-09T12:10:00").toDate(),
-      title: "anna.aguiTU5 - G - Conferencia",
-    },
-    {
-      start: dayjs("2024-04-09T12:00:00").toDate(),
-      end: dayjs("2024-04-09T12:10:00").toDate(),
-      title: "anna.aguiTU5 - G - Conferencia",
-    },
-    {
-      start: dayjs("2024-04-09T12:10:00").toDate(),
-      end: dayjs("2024-04-09T12:20:00").toDate(),
-      title: "anna.aguiTU5 - G - Conferencia",
-    },
-  ];
-
-  const components = {
-    event: (props) => {
-      console.log(props);
-
-      return (
-        <div className="evento">
-          {props.title}{" "}
-          {/*<p>Inicio: {dayjs(props.start).format("HH:mm")}</p> */}
-        </div>
-      );
-    },
-  };
 
   const messages = {
     allDay: "Todo el día",
@@ -69,19 +28,141 @@ function App() {
     noEventsInRange: "Sin eventos",
   };
 
+  const events = [
+    {
+      start: dayjs("2024-04-09T12:00:00").toDate(),
+      end: dayjs("2024-04-09T12:10:00").toDate(),
+      title: "anna.aguiTU5 - G - Confer",
+      type: "por pautar",
+    },
+    {
+      start: dayjs("2024-04-09T12:00:00").toDate(),
+      end: dayjs("2024-04-09T12:10:00").toDate(),
+      title: "anna.aguiTU5 - G - Conferencia",
+      type: "pautada",
+    },
+    {
+      start: dayjs("2024-04-09T12:00:00").toDate(),
+      end: dayjs("2024-04-09T12:10:00").toDate(),
+      title: "anna.aguiTU5 - G - Conferencia",
+      type: "realizada",
+    },
+    {
+      start: dayjs("2024-04-09T12:10:00").toDate(),
+      end: dayjs("2024-04-09T12:20:00").toDate(),
+      title: "anna.aguiTU5 - G - Conferencia",
+      type: "inconvenitente",
+    },
+    {
+      start: dayjs("2024-04-09T12:20:00").toDate(),
+      end: dayjs("2024-04-09T12:30:00").toDate(),
+      title: "anna.aguiTU5 - G - Conferencia",
+      type: "sin conferencia",
+    },
+    {
+      start: dayjs("2024-04-09T12:30:00").toDate(),
+      end: dayjs("2024-04-09T12:40:00").toDate(),
+      title: "anna.aguiTU5 - G - Conferencia",
+      type: "consultada profesional",
+    },
+    {
+      start: dayjs("2024-04-09T12:40:00").toDate(),
+      end: dayjs("2024-04-09T12:50:00").toDate(),
+      title: "anna.aguiTU5 - G - Conferencia",
+      type: "consultada cliente",
+    },
+    {
+      start: dayjs("2024-04-09T12:40:00").toDate(),
+      end: dayjs("2024-04-09T12:50:00").toDate(),
+      title: "anna.aguiTU5 - G - Conferencia",
+      type: "inconvenitente",
+    },
+    {
+      start: dayjs("2024-04-09T12:50:00").toDate(),
+      end: dayjs("2024-04-09T13:00:00").toDate(),
+      title: "anna.aguiTU5 - G - Conferencia",
+      type: "consultada profesional",
+    },
+    {
+      start: dayjs("2024-04-10T12:40:00").toDate(),
+      end: dayjs("2024-04-10T12:50:00").toDate(),
+      title: "anna.aguiTU5 - G - Conferencia",
+      type: "consultada cliente",
+    },
+  ];
+
   const STEP = 10;
   const TIME_SLOTS = 60 / STEP;
 
+  const components = {
+    event: (props) => {
+      console.log(props);
+
+      const { type } = props.event;
+      let eventoClase = "evento";
+
+      switch (type) {
+        case "por pautar":
+          eventoClase += " por-pautar";
+          break;
+        case "pautada":
+          eventoClase += " pautada";
+          break;
+        case "realizada":
+          eventoClase += " realizada";
+          break;
+        case "inconvenitente":
+          eventoClase += " inconvenitente";
+          break;
+        case "sin conferencia":
+          eventoClase += " sin-conferencia";
+          break;
+        case "consultada profesional":
+          eventoClase += " consultada-profesional";
+          break;
+        case "consultada cliente":
+          eventoClase += " consultada-cliente";
+          break;
+        default:
+          break;
+      }
+
+      return (
+        <div className={eventoClase}>
+          <ul>
+            <li>{props.title}</li>
+          </ul>
+          {/* <p>Inicio: {dayjs(props.start).format("HH:mm")}</p> */}
+        </div>
+      );
+    },
+  };
+
+  // Función para el tooltipAccessor personalizado
+  const customTooltipAccessor = (event) => {
+    // Aquí puedes personalizar el tooltip según tu lógica
+    return `Tipo: ${event.type}`; // Ejemplo de tooltip personalizado mostrando el tipo de evento
+  };
+
   return (
     <>
-      <div style={{ height: "95vh", width: "95vw" }}>
+      <div
+        style={{
+          width: "95vw",
+          height: "95vh",
+          backgroundColor: "white",
+          color: "black",
+        }}
+      >
         <Calendar
           localizer={localizer}
-          events={events}
           messages={messages}
+          events={events}
           step={STEP}
           timeslots={TIME_SLOTS}
+          dayLayoutAlgorithm={"no-overlap"}
           components={components}
+          tooltipAccessor={customTooltipAccessor}
         />
       </div>
     </>
